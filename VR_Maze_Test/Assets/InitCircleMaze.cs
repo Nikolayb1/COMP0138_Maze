f7b5ad3969a6e3dbf495f2da3774a817ba60ccb4
Nikolay <mydevacc8@gmail.com>
2021-02-02 19:31:48 +0300

    Circle Maze and Builds

---
+++ VR_Maze_Test/Assets/InitCircleMaze.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitCircleMaze : MonoBehaviour
+{
+    public GameObject outerCell;
+    public GameObject middleCell;
+    public GameObject innerCell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tmp;
+        for (int i = 0; i < 5; i++)
+        {
+            tmp = Instantiate(outerCell, new Vector3(0, 0, 0), Quaternion.Euler(0, 365/5 * i, 0));
+            tmp.transform.parent = transform;
+            tmp = Instantiate(middleCell, new Vector3(0, 0, 0), Quaternion.Euler(0, 365 / 5 * i, 0));
+            tmp.transform.parent = transform;
+            tmp = Instantiate(innerCell, new Vector3(0, 0, 0), Quaternion.Euler(0, 365 / 5 * i, 0));
+            tmp.transform.parent = transform;
+        }
+
+        transform.position = new Vector3(-20, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR_Maze_Test/Assets/LogCell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogCell : MonoBehaviour
+{
+    public bool entered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        entered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "XRRig")
+        {
+            entered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "XRRig")
+        {
+            entered = false;
+        }
+    }
+}
